61b2bd56aed451efe7b3340bfb30f7c75840ff7a
Tomas <tomas.hrobarik@guidevision.cz>
2020-10-18 22:19:30 +0200

    feat: copy to clipboard

--- src/code-block/actions.js
+++ src/code-block/actions.js
@@ -2,6 +2,10 @@ import Prism from "prismjs";
 import {
 	actionTypes
 } from "@servicenow/ui-core";
+import copy from "copy-to-clipboard";
+
+export const COPY_CLICKED = "COPY_CLICKED";
+export const COPY_LABEL_RESETTED = "COPY_LABEL_RESETTED";
 
 export default {
 	actionHandlers: {
@@ -18,6 +22,23 @@ export default {
 			//strip the lead/end newlines so the line numbers are displayed correctly
 			host.shadowRoot.querySelector("code").innerHTML = host.innerHTML.replace(/^\s+|\s+$/g, '');
 			Prism.highlightElement(host.shadowRoot.querySelector("code"));
+		},
+		/**
+		 * Action handler for copy button. It reads the content of the code and copies 
+		 * it to clipboard
+		 */
+		[COPY_CLICKED]: (coeffects) => {
+			const {
+				host, updateState, dispatch
+			} = coeffects;
+			copy(host.shadowRoot.querySelector('code').textContent);
+			updateState({ copyLabel: "Copied!" })
+			clearTimeout(host.timeout);
+			host.timeout = setTimeout(() => { dispatch(COPY_LABEL_RESETTED) }, 1000);
+
+		},
+		[COPY_LABEL_RESETTED]: ({ updateState, host }) => {
+			updateState({ copyLabel: "Copy" });
 		}
 	}
 }
