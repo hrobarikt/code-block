8e4ceaef4d8a534e27c2fc82da428231d661eaf7
Tomas <tomas.hrobarik@guidevision.cz>
2020-10-21 23:20:55 +0200

    refactor: code cleaning 🧹

--- src/code-block/actions.js
+++ src/code-block/actions.js
@@ -6,32 +6,33 @@ import copy from "copy-to-clipboard";
 
 export const COPY_CLICKED = "COPY_CLICKED";
 export const COPY_LABEL_RESETTED = "COPY_LABEL_RESETTED";
-const CODE_HIGHLIGHTED = "CODE_HIGHLIGHTED";
+export const HIGHLIGHT_NEEDED = "HIGHLIGHT_NEEDED";
 
 
 export default {
 	actionHandlers: {
 		[actionTypes.COMPONENT_RENDERED]: ({ dispatch }) => {
-			dispatch(CODE_HIGHLIGHTED);
+			dispatch(HIGHLIGHT_NEEDED);
 		},
 		/**
-		 * Initialize PrismJS highlighting when the component is bootstrapped. It's copying the content
-		 * from the hosting element into the code element placed inside of the shadowDOM
+		 * Initialize PrismJS highlighting when needed. It's 
+		 * copying the content from the hosting element into 
+		 * the code element placed inside of the shadowDOM
 		 *
 		 */
-		[CODE_HIGHLIGHTED]: (coeffects) => {
+		[HIGHLIGHT_NEEDED]: (coeffects) => {
 			const {
 				host,
 				state
 			} = coeffects;
-			console.log('code highlighted')
+			const prismHighlightElement = () => {
+				Prism.highlightElement(host.shadowRoot.querySelector("code"));
+			};
 			//strip the lead/end newlines so the line numbers are displayed correctly
 			host.shadowRoot.querySelector("code").innerHTML = host.innerHTML.replace(/^\s+|\s+$/g, '');
-			import(`prismjs/components/prism-${state.properties.language}`).finally(
-				() => {
-					Prism.highlightElement(host.shadowRoot.querySelector("code"));
-				}
-			)
+			import(`prismjs/components/prism-${state.properties.language}`).then(
+				prismHighlightElement, prismHighlightElement
+			);
 		},
 		/**
 		 * Action handler for copy button. It reads the content of the code and copies 
@@ -61,18 +62,20 @@ export default {
 				copyLabel: "Copy"
 			});
 		},
+		/**
+		 * Setup a mutation observer on text nodes to detect 
+		 * changes within the code and invoke reapplying of
+		 * syntax highlighting
+		 *
+		 */
 		[actionTypes.COMPONENT_BOOTSTRAPPED]: (coeffects) => {
 			const { host, dispatch } = coeffects;
-
-			observer = new MutationObserver(() => {
-				console.log('mutation');
-				dispatch(CODE_HIGHLIGHTED)
+			const observer = new MutationObserver(() => {
+				dispatch(HIGHLIGHT_NEEDED)
 			});
 			host.shadowRoot.querySelector('slot').assignedNodes().forEach(node => {
-
 				observer.observe(node, { characterData: true, childList: true });
 			})
 		},
-
 	}
 }
